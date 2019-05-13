a2e18d46f6211d07299592070f4e43ae1e594092
Dan-Vy Le <danvyle85@gmail.com>
2019-05-13 23:10:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = []
-  [] << element
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
@@ -25,12 +24,10 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = []
   array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  array = []
   array[0]
 end
 
